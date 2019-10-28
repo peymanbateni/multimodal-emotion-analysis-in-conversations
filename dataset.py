c987a5c2e9263dd36d6423f2b963e231200fde7e
Willis <williscpeng@gmail.com>
2019-10-27 19:34:23 -0700

    Added support for loading visual/audio dataset using accessors

--- dataset.py
+++ dataset.py
@@ -10,7 +10,7 @@ import cv2
 from scipy.io import wavfile
 
 def video_to_tensor(video_file):
-    """ Converts a mp4 file into a numpy array"""
+    """ Converts a mp4 file into a pytorch tensor"""
 
     cap = cv2.VideoCapture(video_file)
     frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -42,46 +42,81 @@ class Utterance(object):
         self.file_path = file_path
 
     def get_transcript(self):
+        """
+        Returns a string representation of the transcript
+        """
+        # TODO: Still some issues with parsing the transcript, specifically wrt special symbols
         return self.transcript
 
+    def get_speaker(self):
+        """
+        Returns the integer-mapped speaker id
+        """
+        return self.speaker
+
+    def get_label(self):
+        """
+        Returns a tuple of the integer-mapped label as (emotion, sentiment)
+        """
+
+        return (self.emotion, self.sentiment)
+
     def load_video(self):
+        """
+        Loads the video into memory and converts the frames into a pyTorch tensor
+        """
         return video_to_tensor(self.file_path)
 
     def load_audio(self):
-        return wavfile.read(self.file_path[:-3] + "wav")
+        """
+        reads audio from converted wav file and turns it as a wav tuple (sampling_rate, data)
+        """
+        return wavfile.read(self.file_path.replace("mp4", "wav"))
 
 
 class MELDDataset(Dataset):
+    """
+    Class representing MELD dataset. Initialization is against a csv file and
+    root directory.
+
+    Accessing the dataset via an accessor will load the appropriate video and
+    audio representations in memory and will be returned in the form:
+
+        ([transcript], [video], [audio]) ([emotion_label], [sentiment_label])
+
+    Attributes:
+
+    csv_recods: pandas representation of csv file
+    root_dir: pah to root directory of data
+
+    speaker_mapping: dictionary mapping speaker name to speaker id
+    emotion_mapping: dictionary mapping emotion to emotion id
+    sentiment_mapping: dictionary mapping sentiment to sentiment id
+
+    Methods:
+
+    load_sample_audio(index): loads the audio tuple retrieved from index
+    load_sample_video(index): loads the video tensor retrieved from index
+    """
 
     def __init__(self, csv_file, root_dir):
         self.csv_records = pd.read_csv(csv_file)
         self.root_dir = root_dir
 
-        self.list_of_speakers = list(set(self.csv_records.loc[:, "Speaker"].values.tolist()))
-        self.list_of_emotions = list(set(self.csv_records.loc[:, "Emotion"].values.tolist()))
-        self.list_of_sentiments = list(set(self.csv_records.loc[:, "Sentiment"].values.tolist()))
+        speaker_set = set(self.csv_records.loc[:, "Speaker"].values.tolist())
+        emotion_set = set(self.csv_records.loc[:, "Emotion"].values.tolist())
+        sentiment_set = set(self.csv_records.loc[:, "Sentiment"].values.tolist())
 
-        self.speakers_to_label = {}
-        for i, speaker in enumerate(self.list_of_speakers):
-            self.speakers_to_label[speaker] = i
+        # TODO: we have combination mappings right now (eg. "Monica and Rachael")
+        self.speaker_mapping = {speaker: id for id, speaker in enumerate(speaker_set)}
+        self.emotion_mapping = {emotion: id for id, emotion in enumerate(emotion_set)}
+        self.sentiment_mapping = {sentiment: id for id, sentiment in enumerate(sentiment_set)}
 
-        self.emotions_to_label = {}
-        for i, emotion in enumerate(self.list_of_emotions):
-            self.emotions_to_label[emotion] = i
-
-        self.sentiments_to_label = {}
-        for i, sentiment in enumerate(self.list_of_sentiments):
-            self.sentiments_to_label[sentiment] = i
-
-        print("Speaker mapping: {}".format(self.speakers_to_label))
-        print("Emotion mapping: {}".format(self.emotions_to_label))
-        print("Sentiment mapping: {}".format(self.sentiments_to_label))
+        print("Speaker mapping: {}".format(self.speaker_mapping))
+        print("Emotion mapping: {}".format(self.emotion_mapping))
+        print("Sentiment mapping: {}".format(self.sentiment_mapping))
         #print([self.speakers_to_label[key] for key in self.csv_records.loc[:, "Speaker"].values.tolist()])
 
-        #data_poin
-        #for file_name in os.listdir(self.root_dir):
-        #    if file_name.endswith(".mp4"):
-        #self
         print(self.csv_records.iloc[0:10,:])
 
         self.data = []
@@ -95,20 +130,44 @@ class MELDDataset(Dataset):
 
             utterance = Utterance(
                 transcript,
-                self.speakers_to_label[speaker],
-                self.emotions_to_label[emotion],
-                self.sentiments_to_label[sentiment],
+                self.speaker_mapping[speaker],
+                self.emotion_mapping[emotion],
+                self.sentiment_mapping[sentiment],
                 file_path
             )
             self.data.append(utterance)
 
     def __len__(self):
-        return len(self.csv_records)
+        return len(self.data)
 
     def __getitem__(self, idx):
+        utterances = self.data[idx]
+        if not isinstance(utterances, list):
+            # only single utterance
+            input = ([utterances.get_transcript()], [utterances.load_video()], [utterances.load_audio()])
+            label = utterances.get_label()
+            return input, label
+        else:
+            transcripts = [utterance.get_transcript() for utterance in utterances]
+            video = [utterance.load_video() for utterance in utterances]
+            audio = [utterance.load_audio() for utterance in utterances]
+            emotion_labels = [utterance.get_label()[0] for utterance in utterances]
+            sentiment_labels = [utterance.get_label()[1] for utterance in utterances]
 
-        return None
+        input = (transcripts, video, audio)
+        labels = (emotion_labels, sentiment_labels)
+        return input, labels
 
+    def load_sample_transcript(self, idx):
+        return self.data[idx].get_transcript()
+
+    def load_sample_audio(self, idx):
+        return self.data[idx].load_audio()
+
+    def load_sample_video(self, idx):
+        return self.data[idx].load_video()
+
+    """
     def load_sample_audio(audio_file_address):
         return torchaudio.load(audio_file_address)
 
@@ -133,3 +192,4 @@ class MELDDataset(Dataset):
 
         image_tensors = torch.cat(image_list, dim=0)
         return image_tensors
+    """
--- main.py
+++ main.py
@@ -2,9 +2,24 @@ import torch
 import torch.nn as nn
 from dataset import MELDDataset, Utterance
 
-dataset = MELDDataset("../MELD.Raw/dev_sent_emo.csv", "/MELD.Raw/dev_splits_complete/")
-utterance = Utterance("", 1, 1, 1, "../MELD.Raw/dev_splits_complete/dia0_utt0.mp4")
-print(utterance.load_audio()[1].shape)
-print(utterance.load_video().shape)
+dataset = MELDDataset("../MELD.Raw/dev_sent_emo.csv", "../MELD.Raw/dev_splits_complete/")
+print(len(dataset))
+utterance = dataset[0]
+
+(transcripts, video, audio), (emotion_labels, sentiment_labels) = dataset[0:3]
+
+# Transcripts
+print(transcripts)
+# Video
+print(video[0].shape)
+# Audio
+print(audio[0][1].shape)
+
+# labels
+print(emotion_labels)
+print(sentiment_labels)
+#utterance = Utterance("", 1, 1, 1, "../MELD.Raw/dev_splits_complete/dia0_utt0.mp4")
+#print(utterance.load_audio()[1].shape)
+#print(utterance.load_video().shape)
 
 #dataset_loader.load_image("../MELD.Raw/image.png")
