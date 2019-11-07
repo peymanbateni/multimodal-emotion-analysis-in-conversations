0e2d20af3c0e92ee04431f043b2809b5790d454e
Grigorii <prb578@icloud.com>
2019-11-07 13:42:44 -0800

    weighted adjacency

--- models/dialogue_gcn.py
+++ models/dialogue_gcn.py
@@ -25,7 +25,11 @@ class DialogueGCNC(nn.Module):
         context_embeds = self.context_encoder(indept_embeds)[0]
         attn, relation_matrices = self.construct_edges_relations(context_embeds)
         pred_adj, suc_adj, same_speak_adj, diff_adj_matrix = relation_matrices
-        x = F.relu(self.gc1(x, adj))
+        h = self.pred_rel(x, adj)
+        h += self.suc_rel(x, adj)
+        h += self.same_speak_rel(x, adj)
+        h += self.diff_speak_rel(x, adj)
+
         x = self.gc2(x, adj)
         return F.log_softmax(x, dim=1)
 
@@ -51,20 +55,30 @@ class DialogueGCNC(nn.Module):
         ut_embs = ut_embs.unsqueeze(2)
         raw_attn = torch.matmul(raw_attn, ut_embs)
         attn = torch.softmax(raw_attn, dim=1)
-        relation_matrices = self.build_relation_matrices(ut_embs, speaker_ids)
-        return attn, relation_matrices
+        relation_matrices = self.build_relation_matrices(ut_embs, speaker_ids, attn)
+        return relation_matrices
 
     def build_relation_matrices(self, ut_embs, speaker_ids):
         num_utt = len(ut_embs)
         num_speakers = len(np.unique(speaker_ids))
 
         pred_adj = torch.ones(num_utt, num_utt).triu(0)
-        suc_adj = torch.ones(num_utt, num_utt).tril(-1)
-        same_speak_adj = torch.zeros(num_utt, num_utt)
+        suc_adj = 1 - pred_adj.byte()
+        same_adj_matrix = torch.zeros(num_utt, num_utt)
         for i in range(num_speakers):
             same_speak_indices = speaker_ids == i
             same_adj_matrix[same_speak_indices] = same_speak_indices.long()
         diff_adj_matrix = 1 - same_adj_matrix.byte()
+        # Masking out entries due to att_window_size
         for j in range(num_utt):
-            
-        return pred_adj, suc_adj, same_speak_adj, diff_adj_matrix.long()
+            pred_adj[j, j+1+self.att_window_size:num_utt] = 0
+            suc_adj[j, 0:max(0, j-self.att_window_size)] = 0
+            same_adj_matrix[j, j+1+self.att_window_size:num_utt] = 0
+            same_adj_matrix[j, 0:max(0, j-self.att_window_size)] = 0
+            diff_adj_matrix[j, j+1+self.att_window_size:num_utt] = 0
+            diff_adj_matrix[j, 0:max(0, j-self.att_window_size)] = 0
+        pred_adj *= attn
+        suc_adj *= attn
+        same_adj_matrix *= attn
+        diff_adj_matrix *= attn
+        return pred_adj, suc_adj, same_adj_matrix.long(), diff_adj_matrix.long()
